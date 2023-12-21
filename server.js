15d2863b3bc54d303b4ab82fd22dd0c749f16157
JCovi <joshpcovington@gmail.com>
2023-12-20 20:51:27 -0700

    fixed the path issue

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static('Develop/public'));
+app.use(express.static('public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // API Routes
 app.get('/api/notes', (req, res) => {
   // Read data from db.json and send as response
-  const notes = JSON.parse(fs.readFileSync('Develop/db/db.json', 'utf8'));
+  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
   res.json(notes);
 });
 
@@ -33,7 +33,7 @@ app.post('/api/notes', (req, res) => {
   const newNote = req.body;
 
   // Read existing notes from db.json
-  const notes = JSON.parse(fs.readFileSync('Develop/db/db.json', 'utf8'));
+  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
   // Assign a unique id to the new note
   newNote.id = Date.now().toString();
@@ -42,7 +42,7 @@ app.post('/api/notes', (req, res) => {
   notes.push(newNote);
 
   // Write the updated notes array back to db.json
-  fs.writeFileSync('Develop/db/db.json', JSON.stringify(notes));
+  fs.writeFileSync('db/db.json', JSON.stringify(notes));
 
   // Send the new note as a response
   res.json(newNote);
